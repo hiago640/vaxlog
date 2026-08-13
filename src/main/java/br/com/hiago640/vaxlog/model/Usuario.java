f7b9a738ffbb52868113daea529d673d04ca1cdf
hiago640 <hiago_pereiraborges@hotmail.com>
2026-08-12 21:59:06 -0300

    refactor(user): implementar lombok

--- src/main/java/br/com/hiago640/vaxlog/model/Usuario.java
+++ src/main/java/br/com/hiago640/vaxlog/model/Usuario.java
@@ -1,13 +1,19 @@
 package br.com.hiago640.vaxlog.model;
 
-import java.util.UUID;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
 
 	@Id
@@ -16,48 +22,6 @@ public class Usuario {
 	private String nome;
 	private String email;
 	private String password;
-
-	public Usuario() {
-	}
-
-	public Usuario(Long id, String nome, String email, String password) {
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		this.password = password;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
 	@Override
 	public String toString() {
