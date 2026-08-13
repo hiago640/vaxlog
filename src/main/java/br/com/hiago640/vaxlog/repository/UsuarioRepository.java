f4c677609abd4032e1ee899208ec18f36b8503a9
hiago640 <hiago_pereiraborges@hotmail.com>
2026-08-12 22:02:41 -0300

    feat(user): findByEmail para implementações futuras

--- src/main/java/br/com/hiago640/vaxlog/repository/UsuarioRepository.java
+++ src/main/java/br/com/hiago640/vaxlog/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package br.com.hiago640.vaxlog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.hiago640.vaxlog.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+	Optional<Usuario> findByEmail(String email);
+	
 }
