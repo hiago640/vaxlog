8e27c4f5e684ee03d47853294734ee1b47a72097
hiago640 <hiago_pereiraborges@hotmail.com>
2026-08-12 21:56:27 -0300

    refactor(user) implementar estudos sobre ResponseEntity, e também implementar retorno conforme dto

--- src/main/java/br/com/hiago640/vaxlog/controller/UsuarioController.java
+++ src/main/java/br/com/hiago640/vaxlog/controller/UsuarioController.java
@@ -3,38 +3,37 @@ package br.com.hiago640.vaxlog.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.hiago640.vaxlog.model.Usuario;
-import br.com.hiago640.vaxlog.repository.UsuarioRepository;
+import br.com.hiago640.vaxlog.dto.CreateUserDTO;
+import br.com.hiago640.vaxlog.dto.UserResponseDTO;
 import br.com.hiago640.vaxlog.service.UsuarioService;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/user")
 public class UsuarioController {
 
 	@Autowired
-	private UsuarioRepository repository;
-	
-	@Autowired
 	private UsuarioService service;
 
-	@GetMapping("/listar")
-	public List<Usuario> getAll() {
-		System.out.println("entrou aqui");
-
-		return repository.findAll();
+	@GetMapping
+	public ResponseEntity<List<UserResponseDTO>> listarTodos() {
+		List<UserResponseDTO> usuarios = service.listarTodos();
+		
+		return ResponseEntity.ok(usuarios);
 	}
 
-	@PostMapping("/criar")
-	public Usuario incluirVacina(@RequestBody Usuario usuario) {
-		service.salvar(usuario);
+	@PostMapping
+	public ResponseEntity<UserResponseDTO> criar(@RequestBody CreateUserDTO dto) {
+		UserResponseDTO response = service.salvar(dto);
 
-		return usuario;
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 
 }
